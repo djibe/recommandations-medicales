48ebf641b46ab0867b945530ec0fc4d6788599d2
djibe <djibe89@hotmail.com>
2023-05-28 23:53:00 +0200

    Rewrite search import

---
+++ static/lyra.mjs
@@ -0,0 +1,47 @@
+import {
+  create,
+  search,
+  insertBatch,
+} from "https://cdn.jsdelivr.net/npm/@lyrasearch/lyra@0.4.12/dist/index.js";
+import { stemmer } from "https://cdn.jsdelivr.net/npm/@lyrasearch/lyra@0.4.12/dist/stemmer/fr.min.js";
+const indexResponse = await fetch("/index.json");
+const index = await indexResponse.json();
+
+const searchEngine = await create({
+  schema: {
+    date: 'string',
+    uri: 'string',
+    title: 'string',
+    synonyms: 'string',
+    content: 'string',
+    tags: 'string',
+    section: 'string',
+    annees: 'string',
+    sources: 'string'
+  },
+  defaultLanguage: "french",
+  components: {
+    tokenizer: {
+      stemmingFn: stemmer,
+    },
+  },
+});
+await insertBatch(searchEngine, index);
+
+const searchInput = document.getElementById("search-input");
+["change", "cut", "focus", "input", "paste", "search"].forEach((type) =>
+  searchInput.addEventListener(type, query)
+);
+
+async function query(event) {
+  const searchResponse = await search(searchEngine, {
+    term: event.target.value,
+    properties: "*",
+  });
+  document.getElementById("search-results").innerHTML = searchResponse.hits
+    .map(
+      (i) =>
+        `<a href="${i.document.url}" class="list-group-item list-group-item-action">${i.document.title}</a>`
+    )
+    .join("");
+}
