eeffdaa2c0c954ef93aa327fb65f5641fee96819
djibe <djibe89@hotmail.com>
2024-06-28 12:57:41 +0200

    Packages update

--- build/vnu-jar.mjs
+++ build/vnu-jar.mjs
@@ -2,35 +2,36 @@
 
 /*!
  * Script to run vnu-jar if Java is available.
- * Copyright 2017-2020 The Bootstrap Authors
- * Copyright 2017-2020 Twitter, Inc.
+ * Copyright 2017-2024 The Bootstrap Authors
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  */
 
-'use strict'
+import { execFile, spawn } from 'node:child_process'
+import vnu from 'vnu-jar'
 
-const childProcess = require('child_process')
-const vnu = require('vnu-jar')
-
-childProcess.exec('java -version', (error, stdout, stderr) => {
+execFile('java', ['-version'], (error, stdout, stderr) => {
   if (error) {
-    console.error('Skipping vnu-jar test; Java is missing.')
+    console.error('Skipping vnu-jar test; Java is probably missing.')
+    console.error(error)
     return
   }
+
+  console.log('Running vnu-jar validation...')
 
   const is32bitJava = !/64-Bit/.test(stderr)
 
   // vnu-jar accepts multiple ignores joined with a `|`.
-  // Also note that the ignores are regular expressions.
+  // Also note that the ignores are string regular expressions.
   const ignores = [
     // "autocomplete" is included in <button> and checkboxes and radio <input>s due to
     // Firefox's non-standard autocomplete behavior - see https://bugzilla.mozilla.org/show_bug.cgi?id=654072
     'Attribute “autocomplete” is only allowed when the input type is.*',
     'Attribute “autocomplete” not allowed on element “button” at this point.',
-    // Markup used in Components → Forms → Layout → Form grid → Horizontal form is currently invalid,
-    // but used this way due to lack of support for flexbox layout on <fieldset> element in most browsers
-    'Element “legend” not allowed as child of element “div” in this context.*',
-    // Modern ignore
+    // Per https://www.w3.org/TR/html-aria/#docconformance having "aria-disabled" on a link is
+    // NOT RECOMMENDED, but it's still valid - we explain in the docs that it's not ideal,
+    // and offer more robust alternatives, but also need to show a less-than-ideal example
+    'An “aria-disabled” attribute whose value is “true” should not be specified on an “a” element that has an “href” attribute.',
+    // Modern
     'Attribute “loading” not allowed on element “iframe” at this point.*',
     'Attribute “importance” not allowed on element “img” at this point.*',
     'Attribute “importance” not allowed on element “script” at this point.*',
@@ -39,21 +40,17 @@ childProcess.exec('java -version', (error, stdout, stderr) => {
     'Attribute “fetchpriority” not allowed on element “iframe” at this point.*',
     'Attribute “fetchpriority” not allowed on element “link” at this point.*',
     'Attribute “netlify-honeypot” not allowed on element “form” at this point.*',
-    'CSS: “unicode-range”: Too many values or values are not recognized.*',
     'The “inputmode” attribute is not supported in all browsers. Please be sure to test, and consider using a polyfill.*'
   ].join('|')
 
   const args = [
     '-jar',
-    vnu,
+    `"${vnu}"`,
     '--asciiquotes',
     '--skip-non-html',
-    // Ignore the language code warnings
-    // '--no-langdetect',
     '--Werror',
     `--filterpattern "${ignores}"`,
-    `--also-check-svg`,
-    'public/'
+    `public/`
   ]
 
   // For the 32-bit Java we need to pass `-Xss512k`
@@ -61,7 +58,9 @@ childProcess.exec('java -version', (error, stdout, stderr) => {
     args.splice(0, 0, '-Xss512k')
   }
 
-  return childProcess.spawn('java', args, {
+  console.log(`command used: java ${args.join(' ')}`)
+
+  return spawn('java', args, {
     shell: true,
     stdio: 'inherit'
   })
