a7bc758fdaf8ececef64d9fa1d8541e4cc98e95d
djibe <djibe89@hotmail.com>
2024-07-14 17:40:12 +0200

    Netlify: remove honeypot not working

--- build/vnu-jar.mjs
+++ build/vnu-jar.mjs
@@ -39,7 +39,6 @@ execFile('java', ['-version'], (error, stdout, stderr) => {
     'Attribute “fetchpriority” not allowed on element “img” at this point.*',
     'Attribute “fetchpriority” not allowed on element “iframe” at this point.*',
     'Attribute “fetchpriority” not allowed on element “link” at this point.*',
-    'Attribute “netlify-honeypot” not allowed on element “form” at this point.*',
     'The “inputmode” attribute is not supported in all browsers. Please be sure to test, and consider using a polyfill.*'
   ].join('|')
 
