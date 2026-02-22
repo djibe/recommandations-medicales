6e5f9b66df9b6dc5416ef089ea2bb191c7b4dea1
djibe <djibe89@hotmail.com>
2026-02-22 23:11:03 +0100

    Update key

--- assets/js/orama.mjs
+++ assets/js/orama.mjs
@@ -3,45 +3,46 @@ import { OramaCloud } from "https://esm.sh/@orama/core@1.2.19";
 let client;
 
 async function initializeClient() {
-  /*const response = await fetch('https://recomedicales.fr/.netlify/functions/api');
+  const response = await fetch(
+    "https://recomedicales.fr/.netlify/functions/api",
+  );
   const json = await response.json();
-  const oramaApiKey = json.api;*/
+  const oramaApiKey = json.api;
 
   // Initialisation du client avec la clé API récupérée
   client = new OramaCloud({
-          projectId: "4b046e11-f9a2-4039-83dd-22930b99fa7f",
-          apiKey:
-            "c1_Lhv3nuwNcMmnanr4QUvt4_Znpum4G3C4U0xpgnq-9xAm1x_Ykmyvc067IUk",
-        });
+    projectId: "4b046e11-f9a2-4039-83dd-22930b99fa7f",
+    apiKey: oramaApiKey,
+  });
 }
 
 initializeClient();
 
-const searchInput = document.getElementById('search-input');
-['change', 'cut', 'focus', 'input', 'paste', 'search'].forEach((type) =>
-  searchInput.addEventListener(type, query)
+const searchInput = document.getElementById("search-input");
+["change", "cut", "focus", "input", "paste", "search"].forEach((type) =>
+  searchInput.addEventListener(type, query),
 );
 
 async function query(event) {
   if (!client) {
-    console.error('Orama Search n’est pas encore initialisé.');
+    console.error("Orama Search n’est pas encore initialisé.");
     return;
   }
 
   const searchResponse = await client.search({
     term: event.target.value,
-    properties: '*',
-    mode: 'fulltext',
-    'boost': {
-      'title': 5,
-      'content': 2
-    }
+    properties: "*",
+    mode: "fulltext",
+    boost: {
+      title: 5,
+      content: 2,
+    },
   });
 
-  document.getElementById('search-results').innerHTML = searchResponse.hits
+  document.getElementById("search-results").innerHTML = searchResponse.hits
     .map(
       (i) =>
-        `<a href='${i.document.uri}' class='list-group-item list-group-item-action' role='listitem'>${i.document.title}</a>`
+        `<a href='${i.document.uri}' class='list-group-item list-group-item-action' role='listitem'>${i.document.title}</a>`,
     )
-    .join('');
+    .join("");
 }
