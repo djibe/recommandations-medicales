72a9561ac46482cf907eda0be46353ea03221c4c
djibe <djibe89@hotmail.com>
2026-03-10 18:02:38 +0100

    New: edge-function for bots

---
+++ netlify/edge-functions/block-bots.js
@@ -0,0 +1,36 @@
+// By jeremiak : https://web.archive.org/web/20251224061537/https://www.jeremiak.com/blog/block-bots-netlify-edge-functions/
+// inspired (and taken) from ethan marcotte's blog post: https://ethanmarcotte.com/wrote/blockin-bots/
+// Watch: Observer
+const botUas = [
+  'axios',
+  'Clinicia-Medical-Assistant',
+  'ChatGPT',
+  'ChatGPT-User',
+  'Claude-User',
+  'ClaudeBot',
+  'Go-http-client',
+  'GuzzleHttp',
+  'MJ12bot',
+  'OAI-SearchBot',
+  'okhttp',
+  'PetalBot',
+  'Python-httpx',
+  'python-requests',
+  'quillbot',
+  'YouBot'
+]
+
+export default async (request, context) => {
+  const ua = request.headers.get('user-agent');
+
+  let isBot = false
+
+  botUas.forEach(u => {
+    if (ua.toLowerCase().includes(u.toLowerCase())) {
+      isBot = true
+    }
+  })
+
+  const response = isBot ? new Response(null, { status: 404 }) : await context.next();
+  return response
+};
